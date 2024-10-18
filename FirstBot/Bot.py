d1f0ef59e2660c08de2b99b58aee430003a89eb2
JoviOakz <joao.mateussdc@gmail.com>
2024-10-18 10:38:03 -0300

    fix

--- FirstBot/Bot.py
+++ FirstBot/Bot.py
@@ -2,9 +2,6 @@ import pyautogui as bot
 
 bot.FAILSAFE = True
 
-bot.hotkey('win', 'r')
-bot.write('cmd')
-bot.press('enter')
-bot.sleep(0.75)
-bot.write('notepad')
+bot.click(1780, 20)
+bot.hotkey('alt', 'f4')
 bot.press('enter')
